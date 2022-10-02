d8c761f94e47329537778fd0a15b7c1996ebc6a5
Jonas Brossmann <jbrossmann@coconut-capital.com>
2022-10-02 13:07:04 +0200

    deploy github

--- web/src/domains/movement/floor.tsx
+++ web/src/domains/movement/floor.tsx
@@ -1,10 +1,11 @@
 import * as THREE from "three"
 import { useLoader } from "@react-three/fiber"
 import { Suspense, useEffect, useRef, useState } from "react"
-import { useGLTF, useTexture, useFBX } from "@react-three/drei"
+import { useGLTF, useTexture, useFBX, Clone } from "@react-three/drei"
 import { WorldState } from "./useMovementStore"
 import bookStore0StaticObject from "../../../public/dataStaticObjects/bookstore0.json"
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
+import { Group } from "three"
 
 export default function Floor(props: { world: WorldState | null }) {
     const [colorMfloorNormalTexture] = useTexture(["./textures/dirt/normal.jpg"])
@@ -16,22 +17,14 @@ export default function Floor(props: { world: WorldState | null }) {
     colorMfloorNormalTexture.wrapS = THREE.RepeatWrapping
     colorMfloorNormalTexture.wrapT = THREE.RepeatWrapping
     const scene = useFBX("./models/bookstorefull.fbx")
-    const floor = useRef()
+    const floor = useRef<Group>(null)
     return (
         <>
-{/*             <mesh rotation={[-Math.PI / 2, 0, Math.PI / 2]} position={[0, 10, 0]}>
+            {/*             <mesh rotation={[-Math.PI / 2, 0, Math.PI / 2]} position={[0, 10, 0]}>
                 <planeGeometry args={[props.world?.width ?? 1424, props.world?.height ?? 1088]} />
                 <meshStandardMaterial map={colorMap} normalMap={colorMfloorNormalTexture}></meshStandardMaterial>
             </mesh> */}
-            <Suspense fallback={null}>
-                <primitive
-                    ref={floor}
-                    rotation={[0, 0, 0]}
-                    object={scene}
-                    scale={[2.6, 2.8, 3.1]}
-                    position={[20, 0, -20]}
-                />
-            </Suspense>
+            <Clone ref={floor} rotation={[0, 0, 0]} object={scene} scale={[2.6, 2.8, 3.1]} position={[20, 0, -20]} />
             <ambientLight />
         </>
     )
