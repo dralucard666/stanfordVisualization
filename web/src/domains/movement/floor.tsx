68656e8b7c7b15a6870f03e06526ccd9d6e244a5
Jonas Brossmann <jbrossmann@coconut-capital.com>
2022-10-02 16:40:42 +0200

    tdrhdthrfnjdfthnhdn

--- web/src/domains/movement/floor.tsx
+++ web/src/domains/movement/floor.tsx
@@ -9,25 +9,31 @@ import { Group } from "three"
 
 export default function Floor(props: { world: WorldState | null }) {
     const [colorMfloorNormalTexture] = useTexture(["./textures/dirt/normal.jpg"])
-    const [colorMap] = useTexture([props.world?.image ?? "./textures/dirt/bookstore.jpg"])
+    const [colorMap] = useTexture(["./textures/dirt/bookstore.jpg"])
     colorMap.rotation = Math.PI
     colorMap.encoding = THREE.sRGBEncoding
     colorMap.wrapS = THREE.RepeatWrapping
     colorMap.wrapT = THREE.RepeatWrapping
     colorMfloorNormalTexture.wrapS = THREE.RepeatWrapping
     colorMfloorNormalTexture.wrapT = THREE.RepeatWrapping
-    const scene = useFBX("./models/bookstore2.fbx")
+    const scene = useGLTF("./models/bookstore.glb")
     const floor = useRef<Group>(null)
     return (
         <>
-            {/*             <mesh rotation={[-Math.PI / 2, 0, Math.PI / 2]} position={[0, 10, 0]}>
+            <mesh rotation={[-Math.PI / 2, 0, Math.PI]} position={[0, 10, 0]}>
                 <planeGeometry args={[props.world?.width ?? 1424, props.world?.height ?? 1088]} />
                 <meshStandardMaterial map={colorMap} normalMap={colorMfloorNormalTexture}></meshStandardMaterial>
-            </mesh> */}
-            <Clone ref={floor} rotation={[0, 0, 0]} object={scene} scale={[2.6, 2.8, 3.1]} position={[20, 0, -20]} />
+            </mesh>
+            <Clone
+                ref={floor}
+                rotation={[0, 0, 0]}
+                object={scene.scene}
+                scale={[260, 280, 310]}
+                position={[-20, 0, -20]}
+            />
             <ambientLight />
         </>
     )
 }
 
-useFBX.preload("./models/bookstore2.fbx")
+useGLTF.preload("./models/bookstore.glb")
