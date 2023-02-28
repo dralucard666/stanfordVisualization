7c490334a10ab08d8a185dca9103d7b4f2fa7edf
Jonas Brossmann <jbrossmann@coconut-capital.com>
2023-02-28 15:17:15 +0100

    changes in index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -48,7 +48,7 @@ export default function Movement() {
     return (
         <>
             <Head>
-                <title>CGV | Shape Editorrrrrrrrrrrrrrrrr</title>
+                <title>CGV | Shape Editor</title>
                 <meta name="description" content=""></meta>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
